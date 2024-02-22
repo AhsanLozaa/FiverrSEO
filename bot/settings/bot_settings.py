2b9111387869e1dda65fe57ab05689699beb7aa6
AhsanLozaa <ahsanmohamed97@gmail.com>
2024-02-22 22:03:00 +0530

    updated

--- bot/settings/bot_settings.py
+++ bot/settings/bot_settings.py
@@ -3,6 +3,7 @@ from scraper.custom_web_driver import CustomWebDriver
 
 @dataclass
 class BotSettings:
+    custom_web_driver: CustomWebDriver = field(default_factory=CustomWebDriver)
     
     def some_func(self):
         print("This is bot settings class")
---
+++ bot/settings/constants.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class BotConstants(Enum):
+    NAV_DASH_BOARD_ANCHOR_TAG = "seller-main-item"
+    NAV_DASH_BOARD_ANCHOR_TAG_TEXT = "dashboard"
